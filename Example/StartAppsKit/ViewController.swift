a63dc3844ee24a1289c80ba997c4dc4dfb604555
Gabriel Lanata <gabriellanata@gmail.com>
2015-09-18 16:20:14 -0500

    Initial commit

---
+++ Example/StartAppsKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StartAppsKit
+//
+//  Created by Gabriel Lanata on 09/18/2015.
+//  Copyright (c) 2015 Gabriel Lanata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
